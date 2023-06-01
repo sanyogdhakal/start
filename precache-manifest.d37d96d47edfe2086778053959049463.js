153bab0bfbf829a6e9355d14f4d8bfaccd447fcf
Sanyog <sanyogdhakal21@gmail.com>
2023-06-01 16:44:42 +0545

    Updates

---
+++ precache-manifest.d37d96d47edfe2086778053959049463.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3b7c8a7311a6ca869c6",
+    "url": "/start/static/css/main.9d6f4d00.chunk.css"
+  },
+  {
+    "revision": "c3b7c8a7311a6ca869c6",
+    "url": "/start/static/js/main.c3b7c8a7.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/start/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "56e6ca2fc6de37ea2890",
+    "url": "/start/static/js/2.56e6ca2f.chunk.js"
+  },
+  {
+    "revision": "b829fe81892ddb9540d1",
+    "url": "/start/static/js/runtime~main.b829fe81.js"
+  },
+  {
+    "revision": "9f29fbacf53be99f5a44de5609a9252e",
+    "url": "/start/index.html"
+  }
+];
